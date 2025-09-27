5308c3b9f602e0e20b5f9f8cf2ecc59d2af4599b
podajaiganesh <podajaiganesh1234@gmail.com>
2025-09-27 07:24:36 +0530

    Your commit message

--- vite.config.js
+++ vite.config.js
@@ -1,8 +1,9 @@
 import { defineConfig } from 'vite'
 import react from '@vitejs/plugin-react-swc'
 
-
 export default defineConfig({
-  base: '/feedback/', 
+  // This tells Vite to build assets for the root of a domain ('/'),
+  // which is correct for our Docker setup.
+  base: '/', 
   plugins: [react()],
 })
